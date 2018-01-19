07d23020114f46529d1e8f3a2bcbfc48b1ff8ab2
Mai Hameed <maihameed@hotmail.com>
2018-01-19 10:13:52 -0500

    Updated speed ratios

--- Updated_Code.c
+++ Updated_Code.c
@@ -2,15 +2,13 @@
 task main()
 {
 	while (true){
-	motor[port2] = vexRT[Ch2];
-	motor[port3] = vexRT[Ch2];
-	motor[port4] = vexRT[Ch3];
-	motor[port5] = vexRT[Ch3];
-	motor[port6] = vexRT[Btn8D]*127;
-	motor[port6] = -vexRT[Btn8U]*127;
-	motor[port7] = vexRT[Btn7D]*127;
-	motor[port8] = vexRT[Btn7D]*127;
-}
-
-
+		motor[port2] = vexRT[Ch2]*0.7;
+		motor[port3] = vexRT[Ch2]*0.7;
+		motor[port4] = vexRT[Ch3]*0.7;
+		motor[port5] = vexRT[Ch3]*0.7;
+		motor[port6] = vexRT[Btn8D]*127;
+		motor[port6] = -vexRT[Btn8U]*127;
+		motor[port7] = vexRT[Btn7D]*127*0.1;
+		motor[port8] = vexRT[Btn7D]*127*0.1;
+	}
 }
